d4d67aef8678626353e91a17e8336559ac4c8ea3
jeremyrrose <jeremy.r.rose@gmail.com>
2020-10-23 02:16:54 -0400

    fixed pagination

--- p4backend/settings.py
+++ p4backend/settings.py
@@ -130,7 +130,7 @@ REST_FRAMEWORK = {
     ),
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 3
+    'PAGE_SIZE': 100
 }
 
 # Internationalization
